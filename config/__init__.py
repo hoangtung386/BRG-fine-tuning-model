a0f43beb63459f6e9c9af9bef5d936ca84b93039
hoangtung386 <levuhoangtung1542003@gmail.com>
2026-04-16 17:58:07 +0700

    The batch size has been changed to avoid out-of-memory issues

--- config/__init__.py
+++ config/__init__.py
@@ -1,7 +1,7 @@
 class Config:
     IMG_SIZE = 1024
-    BATCH_SIZE = 5
-    NUM_WORKERS = 2
+    BATCH_SIZE = 4
+    NUM_WORKERS = 4
     NUM_EPOCHS = 30
     LEARNING_RATE = 2e-5
     LEARNING_RATE_ENCODER = 5e-6
